8e59ef378b0ed484245fe8255e86c7df7866e279
Omar Gonzalez <omar.gonzalez@worldofgoodbrands.com>
2024-02-16 02:32:49 -0800

    feat: added the rest of the commands and scaffolded the functions to handle all commands

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ type AsyncResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Subcommand)]
 enum Commands {
+    /// Start Twitch Chat client
     Chat {
         /// Your Twitch username
         #[arg(long, short = 'n', env = "TWITCH_NAME")]
@@ -21,7 +22,60 @@ enum Commands {
         /// Your Twitch app client ID
         #[arg(long, short, env = "CLIENT_ID")]
         client_id: String,
-    }
+    },
+
+    /// List chat commands
+    ListCommands,
+
+    /// Add a chat command
+    AddCommand {
+        /// The name of the chat command
+        #[arg(long, short)]
+        command_name: String,
+
+        /// The message to send for the chat command
+        #[arg(long, short)]
+        message: String,
+    },
+
+    /// Remove a chat command
+    RemoveCommand {
+        /// The name of the chat command to remove
+        #[arg(long, short)]
+        command_name: String
+    },
+
+    /// List chat announcements
+    ListAnnouncements,
+
+    /// Add a recurring chat announcement
+    AddAnnouncement {
+        /// The name of the chat announcement
+        #[arg(long, short)]
+        announcement_name: String,
+
+        /// How often to send the announcement 
+        #[arg(long, short)]
+        timing: String,
+
+        /// The message body for the announcement
+        #[arg(long, short)]
+        message: String,
+    },
+
+    /// Remove a recurring chat announcement
+    RemoveAnnouncement {
+        /// The name of the chat announcement
+        #[arg(long, short)]
+        announcement_name: String,
+    },
+
+    /// Send a chat message
+    SendMessage {
+        /// The message body 
+        #[arg(long, short)]
+        message: String,
+    },
 }
 
 #[derive(Parser)]
@@ -38,6 +92,40 @@ async fn start_chat(twitch_name: String, oauth_token: String, client_id: String)
     Ok(())
 }
 
+fn list_commands() {
+    println!("Chat commands go here");
+    todo!();
+}
+
+fn add_command(command_name: String, message: String) {
+    println!("Add command here {} {}", command_name, message);
+    todo!();
+}
+
+fn remove_command(command_name: String) {
+    println!("Remove command here {}", command_name);
+    todo!();
+}
+
+fn list_announcements() {
+    println!("List announcements goes here");
+    todo!();
+}
+
+fn add_announcement(announcement_name: String, timing: String, message: String) {
+    println!("Add announcement here {} {} {}", announcement_name, timing, message);
+    todo!();
+}
+
+fn remove_announcement(announcement_name: String) {
+    println!("Remove announcement here {}", announcement_name);
+    todo!();
+}
+
+fn send_message(message: String) {
+    println!("Send message {}", message);
+    todo!();
+}
 
 #[tokio::main]
 async fn main() {
@@ -46,7 +134,22 @@ async fn main() {
 
     let cli = Cli::parse();
     let _ = match cli.commands {
-        Commands::Chat{ twitch_name, oauth_token, client_id } => start_chat(twitch_name, oauth_token, client_id).await
+        Commands::Chat{ twitch_name, oauth_token, client_id } => { 
+            let _ = start_chat(twitch_name, oauth_token, client_id).await;
+        },
+        Commands::ListCommands => { list_commands(); },
+        Commands::AddCommand{ command_name, message } => { add_command(command_name, message); },
+        Commands::RemoveCommand{ command_name } => { remove_command(command_name); },
+        Commands::ListAnnouncements => { list_announcements(); },
+        Commands::AddAnnouncement{ announcement_name, timing, message } => {
+            add_announcement(announcement_name, timing, message);
+        },
+        Commands::RemoveAnnouncement{ announcement_name } => {
+            remove_announcement(announcement_name);
+        },
+        Commands::SendMessage{ message } => {
+            send_message(message);
+        },
     };
 }
 
